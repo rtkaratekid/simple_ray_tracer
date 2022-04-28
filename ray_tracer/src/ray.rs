98844be8a1c9ee4df59268aa33bcd202797d432a
Tristan <mayfieldtristan@gmail.com>
2022-04-28 17:48:50 -0600

    I almost have the ray working

---
+++ ray_tracer/src/ray.rs
@@ -0,0 +1,17 @@
+use crate::vec3::{Point3D, Vec3D};
+
+pub struct Ray {
+    pub origin: Point3D,
+    pub direction: Vec3D,
+}
+
+impl Ray {
+
+    pub fn new(origin: Point3D, direction: Vec3D) -> Self {
+        Self { origin, direction }
+    }
+
+    pub fn at(self, t: f64) -> Point3D {
+        self.origin + self.direction * t
+    }
+}
